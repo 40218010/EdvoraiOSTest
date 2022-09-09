833b8cc75733f38b8d193f97f84c3f7e0201f3a3
Ya Hsuan LIN <40218010@stu.nkuht.edu.tw>
2022-09-09 21:18:57 +0800

    Add ImagePickerView package

---
+++ EdvoraiOSTest/EdvoraiOSTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdvoraiOSTestApp.swift
+//  EdvoraiOSTest
+//
+//  Created by 林大屍 on 2022/9/9.
+//
+
+import SwiftUI
+
+@main
+struct EdvoraiOSTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
